5118c117165dbb3e3771ae8cfd1057cc68eb7de2
Daniel Pino <dapino@outlook.com>
2018-11-15 19:01:06 -0500

    WIP: footer and bottom bar

---
+++ src/components/footer/layout.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "../../_styles/components/footer.scss"
+
+const Footer = props => (
+    <footer className="Footer">
+            <div className="Footer__logos">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-4">
+                            <img src="" alt="Logo Portal"/>
+                        </div>
+                        <div className="col-4">
+                            <img src="" alt="Logo Catálogo"/>
+                        </div>
+                        <div className="col-4">
+                            <img src="" alt="Logo Colecciones"/>
+                        </div>
+                        <div className="col-4">
+                            <img src="" alt="Logo Listas"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="Footer__bar">
+                <div className="container">
+                    barra inferior
+                </div>
+            </div>
+    </footer>
+)
+
+export default Footer
