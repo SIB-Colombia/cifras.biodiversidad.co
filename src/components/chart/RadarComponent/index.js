9767263e6415ae60ed2154e790d79daeadecbd5a
Daniel Pino <dapino@outlook.com>
2018-11-22 16:35:37 -0500

    WIP: Radar chart implementation

---
+++ src/components/chart/RadarComponent/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Radar } from 'react-chartjs-2';
+
+const data = {
+    labels: ['Amenazadas', 'CITES', 'Endémicas', 'Migratorias', 'Exóticas'],
+    datasets: [
+        {
+            label: 'Colombia',
+            backgroundColor: 'rgba(179,181,198,0.2)',
+            borderColor: 'rgba(179,181,198,1)',
+            pointBackgroundColor: 'rgba(179,181,198,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(179,181,198,1)',
+            data: [65, 59, 90, 81, 86]
+        },
+        {
+            label: 'Santander',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            pointBackgroundColor: 'rgba(255,99,132,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(255,99,132,1)',
+            data: [28, 48, 40, 19, 30]
+        }
+    ]
+};
+
+class RadarComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Radar Example</h2>
+                <Radar data={data} />
+            </div>
+        );
+    }
+}
+
+export default RadarComponent
