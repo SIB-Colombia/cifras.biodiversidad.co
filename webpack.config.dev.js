5f45e3c6c90f765d69bc402a3909697a87d38eab
Daniel Pino <dapino@outlook.com>
2018-11-22 16:33:37 -0500

    CHORE: Install React-table and chart.js

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -29,7 +29,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|css)$/,
                 use: ["style-loader", "css-loader", "sass-loader" ]
             },
             {
