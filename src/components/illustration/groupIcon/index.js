5d1ab57c40920ea5fd05e9acc23b6cb44a17430b
Daniel Pino <dapino@outlook.com>
2019-01-31 18:01:51 -0500

    WIP: icons structure

---
+++ src/components/illustration/groupIcon/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+
+class GroupIconComponent extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>icono</p>
+            </div>
+        );
+    }
+}
+
+export default GroupIconComponent
+
