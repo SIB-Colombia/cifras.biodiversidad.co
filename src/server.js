560646203f2ea609486e40ca5197bfd75d0b9a94
Daniel Pino <dapino@outlook.com>
2019-02-06 20:04:04 -0500

    Update server config

--- src/server.js
+++ src/server.js
@@ -1,23 +1,35 @@
-import express from 'express'
+import Express from 'express';
 import React from 'react'
-import App from '../dist/ssr/app'
 import { StaticRouter } from 'react-router'
 import reactDOMServer from 'react-dom/server'
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from 'apollo-link-http';
+import App from '../dist/ssr/app'
 
-const app = express();
-app.use(express.static('dist'))
-app.use('/images', express.static('static'))
+const app = Express();
+
+app.use(Express.static('dist'))
+
+const client = new ApolloClient({
+    ssrMode: true,
+    link: new HttpLink({ uri: 'http://158.69.59.122:8001/graphql' }),
+    cache: new InMemoryCache(),
+});
 
 app.get('*', (req, res) => {
     const html = reactDOMServer.renderToString(
-        <StaticRouter
-            location={req.url}
-            context={{
-                logged: true
-            }}
-        >
-            <App />
-        </StaticRouter>
+        <ApolloProvider client={client}>
+            <StaticRouter
+                location={req.url}
+                context={{
+                    logged: true
+                }}
+            >
+                <App />
+            </StaticRouter>
+        </ApolloProvider>
     )
     res.write(`
         <!doctype html>
@@ -27,13 +39,13 @@ app.get('*', (req, res) => {
             <meta name="viewport"
                   content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Ventana Regional Santander</title>
-            <!--<link rel="stylesheet" href="/css/app.css">-->
+            <title>Ventana Regional</title>
+            <link rel="stylesheet" href="./css/app.css">
         </head>
         <body>
             <div id="app">${html}</div>
-            <script src="/js/app.js"></script>
         </body>
+       
         </html>
     `)
     res.end()
