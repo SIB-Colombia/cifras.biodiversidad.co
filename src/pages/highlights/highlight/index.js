05cbdda846f5933aa06ff021b6dcc38a8a7692d0
Daniel Pino <dapino@outlook.com>
2019-02-06 20:09:27 -0500

    Update highlights format

--- src/pages/highlights/highlight/index.js
+++ src/pages/highlights/highlight/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-const Highlight = props => (
-    <div className={`Hightlight col-${props.size} ${props.color} ${props.align}` } style={{background: `url(${props.background})`}}>
-        <Link to={props.link}>
-            <h2 className="Hightlight__number">{props.number}</h2>
-            <p className="Hightlight__title">{props.title}</p>
-            <p className="Hightlight__text">{props.text}</p>
-        </Link>
-    </div>
-)
+const Highlight = props => {
+    const text = {__html: props.text };
+    return (
+        <div className={`col-${props.size} ` } >
+            <div className={`Highlight ${props.color} ${props.align}`} style={{backgroundImage: `url(${props.background})`}}>
+                <Link to={props.link}>
+                    <p className="Highlight__text" dangerouslySetInnerHTML={text}></p>
+                </Link>
+            </div>
+        </div>
+    )
+}
 
 export default Highlight
 
--- src/pages/highlights/layout.js
+++ src/pages/highlights/layout.js
@@ -2,12 +2,16 @@ import React from 'react'
 import "../../_styles/components/highlight.scss"
 
 const HighlightsLayout = props => (
-    <section className="HighlightsLayout container">
-        <div className="row">
-            <h1>Cifras destacadas</h1>
+    <section className="HighlightsLayout">
+        <div className="container">
+            <div className="row">
+                <h1 className="underline-title">Cifras destacadas</h1>
+            </div>
         </div>
-        <div className="row">
-            {props.children}
+        <div className="container--full">
+            <div className="row">
+                {props.children}
+            </div>
         </div>
     </section>
 )
