1625fa0b8fbe3a163382c0a84e7fb2b97ac6823b
Daniel Pino <dapino@outlook.com>
2019-02-12 17:29:32 -0500

    Update router

---
+++ src/pages/router.js
@@ -0,0 +1,33 @@
+import React, { Fragment, Component, PropTypes } from 'react'
+import Home from './home';
+import Navbar from '../components/navbar';
+import Highlights from './highlights';
+import Groups from './groups';
+import Geo from './geo';
+import Companies from './companies';
+import About from "./about";
+import Footer from '../components/footer/index';
+import NotFound from './not-found';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+class Router extends Component {
+    render () {
+        return (
+            <Fragment>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/destacados" component={Highlights}/>
+                    <Route exact path="/grupos" component={Groups}/>
+                    <Route exact path="/municipios" component={Geo}/>
+                    <Route exact path="/entidades" component={Companies}/>
+                    <Route exact path="/sobre-el-portal" component={About}/>
+                    <Route component={NotFound}/>
+                </Switch>
+                <Footer/>
+            </Fragment>
+        )
+    }
+}
+
+export default Router
