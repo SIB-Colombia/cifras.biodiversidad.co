35e8d3eef86768ae7d95edfe081fb4129f6a2bc7
Daniel Pino <dapino@outlook.com>
2019-02-15 12:53:14 -0500

    Responsive Styles

--- src/components/slider/slide/index.js
+++ src/components/slider/slide/index.js
@@ -3,6 +3,7 @@ import SlideLayout from "./layout";
 
 class Slide extends Component {
     render () {
+        const subtitle = {__html: this.props.subtitle };
         let slideBackground = {
             backgroundImage: 'url(' + this.props.background + ')',
         };
@@ -11,14 +12,14 @@ class Slide extends Component {
                 <div className="SlideLayout__slide">
                     <div className="row">
                         <div className="col-1"></div>
-                        <div className="col-10">
-                            <img src={this.props.svg} alt=""/>
+                        <div className="col-6">
+                            <h3 className="SlideLayout__title" dangerouslySetInnerHTML={subtitle}></h3>
                             {
                                 this.props.credit &&
                                 <p className="SlideLayout__credit">{this.props.credit}</p>
                             }
                         </div>
-                        <div className="col-1"></div>
+                        <div className="col-5"></div>
                     </div>
                 </div>
             </SlideLayout>
