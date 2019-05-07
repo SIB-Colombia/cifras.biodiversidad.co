7f3eb905870e9857620068bd7ec527dd58f3f868
Daniel Pino <dapino@outlook.com>
2019-05-07 15:51:41 -0500

    Hide menú on mouse leave

--- src/components/navbar/menu/index.js
+++ src/components/navbar/menu/index.js
@@ -3,11 +3,14 @@ import { Link, NavLink } from 'react-router-dom'
 import "../../../_styles/components/menu.scss"
 
 const Menu = props => (
-    <div className="MainMenu">
+    <div
+        className="MainMenu"
+        onMouseLeave={props.handleClick}
+    >
         <ul>
             {
                 props.items.map( item => (
-                    <li className="MainMenu__item" key={item.get('id')} onClick={Map.handleClick}>
+                    <li className="MainMenu__item" key={item.get('id')} onClick={props.handleClick}>
                         <NavLink exact to={item.get('url')} activeClassName="is-active">
                             {item.get('title')}
                         </NavLink>
